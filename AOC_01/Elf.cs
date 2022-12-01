fc21310224902f9390165fe531c3e4e1f0deaf2f
DanielRiddersporre <daniel.riddersporre@icloud.comgit config --global user.name DanielRiddersporregit config --global user.email daniel.riddersporre@icloud.com>
2022-12-01 10:27:02 +0100

    First commit

---
+++ AOC_01/Elf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_0101
+{
+    internal class Elf
+    {
+        public Elf(int totalCalories)
+        {
+            CarriedCalories = totalCalories;
+        }
+        public int CarriedCalories { get; set; }
+    }
+}
---
+++ AOC_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using AOC_0101;
+using System.Runtime.CompilerServices;
+
+
+List<Elf> listOfElfs = new List<Elf>();
+string filePath = "C:\\Users\\Danie\\source\\repos\\AdventOfCode2022\\AOC_0101\\resources\\index.txt";
+
+// Collect the needed data and process it as needed.
+try
+{
+    using (StreamReader sr = new StreamReader(filePath))
+    {
+        string line;
+        int number = 0;
+
+        while((line = sr.ReadLine()) != null)
+        {
+            int.TryParse(line, out int result);
+            number = number + result;
+
+            if(result == 0)
+            {
+                Elf elf = new Elf(number);
+                listOfElfs.Add(elf);
+                number = 0;
+            }
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+List<Elf> sortedList = listOfElfs.OrderByDescending(elf => elf.CarriedCalories).ToList();
+
+// Asnwer to the first part
+Console.WriteLine($"Most calories carried: {sortedList[0].CarriedCalories}");
+// Answer to the second part
+Console.WriteLine($"Total amount of carried calories among top three elfs: {sortedList[0].CarriedCalories + sortedList[1].CarriedCalories + sortedList[2].CarriedCalories}");
+
