0c97655171197a96e72fce6797ab1ccc855ea640
DanielRiddersporre <daniel.riddersporre@icloud.comgit config --global user.name DanielRiddersporregit config --global user.email daniel.riddersporre@icloud.com>
2022-12-04 13:23:21 +0100

    Day four is done and the code is fairly good!

--- AOC_04/Program.cs
+++ AOC_04/Program.cs
@@ -1,13 +1,28 @@
 ﻿
-var exampleFile = "..\\..\\..\\resources\\example_input.txt";
+using AOC_04;
+using System.Security.Cryptography.X509Certificates;
+
+var exampleFile = @"..\\..\\..\\resources\\example_input.txt";
+var file = @"..\\..\\..\\resources\\input.txt";
 string line;
+List<List<Section>> sections = new List<List<Section>>();
 try
 {
-    using (var reader = new StreamReader(exampleFile))
+    using (var reader = new StreamReader(file))
     {
         while((line = reader.ReadLine()) != null)
         {
-            Console.WriteLine(line);
+            string[] firstSplit = line.Split(",");
+            List<Section> pair = new List<Section>();
+            foreach(var part in firstSplit)
+            {
+                string[] secondSplit = part.Split("-");
+                Section section = new Section();
+                section.start = int.Parse(secondSplit[0]);
+                section.end = int.Parse(secondSplit[1]);
+                pair.Add(section);
+            }
+            sections.Add(pair);
         }
     }
 }
@@ -15,3 +30,9 @@ catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
+
+var checkA = sections.Where(x => (x[0].start >= x[1].start && x[0].end <= x[1].end) || (x[1].start >= x[0].start && x[1].end <= x[0].end)).ToList();
+var checkB = sections.Where(x => (x[0].start <= x[1].end && x[0].end >= x[1].start)).ToList();
+
+Console.WriteLine(checkA.Count);
+Console.WriteLine(checkB.Count);
---
+++ AOC_04/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_04
+{
+    internal class Section
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+    }
+}
