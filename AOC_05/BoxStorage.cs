79002c6fd565e34f17907c87a230c17fc7043987
DanielRiddersporre <daniel.riddersporre@icloud.comgit config --global user.name DanielRiddersporregit config --global user.email daniel.riddersporre@icloud.com>
2022-12-06 00:24:54 +0100

    Today the code aint pretty but it does the job. Spent way to much time on this one.. but it was fun clearing day 5!

---
+++ AOC_05/BoxStorage.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_05
+{
+    internal class BoxStorage
+    {
+        List<string> ColumnOne{ get; set; }
+        List<string> ColumnTwo { get; set; }
+        List<string> ColumnThree { get; set; }
+        List<string> ColumnFour { get; set; }
+        List<string> ColumnFive { get; set; }
+        List<string> ColumnSix { get; set; }
+        List<string> ColumnSeven { get; set; }
+        List<string> ColumnEight { get; set; }
+        List<string> ColumnNine { get; set; }
+
+        public BoxStorage(int x, int y)
+        {
+            List<string> columnOne = new List<string>();
+            columnOne.Add("R");
+            columnOne.Add("N");
+            columnOne.Add("P");
+            columnOne.Add("G");
+            List<string> columnTwo = new List<string>();
+            columnTwo.Add("T");
+            columnTwo.Add("J");
+            columnTwo.Add("B");
+            columnTwo.Add("L");
+            columnTwo.Add("C");
+            columnTwo.Add("S");
+            columnTwo.Add("V");
+            columnTwo.Add("H");
+            List<string> columnThree = new List<string>();
+            columnThree.Add("T");
+            columnThree.Add("D");
+            columnThree.Add("B");
+            columnThree.Add("M");
+            columnThree.Add("N");
+            columnThree.Add("L");
+            List<string> columnFour = new List<string>();
+            columnFour.Add("R");
+            columnFour.Add("V");
+            columnFour.Add("P");
+            columnFour.Add("S");
+            columnFour.Add("B");
+            List<string> columnFive = new List<string>();
+            columnFive.Add("G");
+            columnFive.Add("C");
+            columnFive.Add("Q");
+            columnFive.Add("S");
+            columnFive.Add("W");
+            columnFive.Add("M");
+            columnFive.Add("V");
+            columnFive.Add("H");
+            List<string> columnSix = new List<string>();
+            columnSix.Add("W");
+            columnSix.Add("Q");
+            columnSix.Add("S");
+            columnSix.Add("C");
+            columnSix.Add("D");
+            columnSix.Add("B");
+            columnSix.Add("J");
+            List<string> columnSeven = new List<string>();
+            columnSeven.Add("F");
+            columnSeven.Add("Q");
+            columnSeven.Add("L");
+            List<string> columnEight = new List<string>();
+            columnEight.Add("W");
+            columnEight.Add("M");
+            columnEight.Add("H");
+            columnEight.Add("T");
+            columnEight.Add("D");
+            columnEight.Add("L");
+            columnEight.Add("F");
+            columnEight.Add("V");
+            List<string> columnNine = new List<string>();
+            columnNine.Add("L");
+            columnNine.Add("P");
+            columnNine.Add("B");
+            columnNine.Add("V");
+            columnNine.Add("M");
+            columnNine.Add("J");
+            columnNine.Add("F");
+
+            ColumnOne = columnOne;
+            ColumnTwo = columnTwo;
+            ColumnThree = columnThree;
+            ColumnFour = columnFour;
+            ColumnFive = columnFive;
+            ColumnSix = columnSix;
+            ColumnSeven = columnSeven;
+            ColumnEight = columnEight;
+            ColumnNine = columnNine;
+        }
+
+        public List<string> GetCorrectColumn(int number)
+        {
+            switch (number)
+            {
+                case 1: return ColumnOne;
+                    case 2: return ColumnTwo;
+                    case 3: return ColumnThree;
+                    case 4: return ColumnFour;
+                    case 5: return ColumnFive;
+                    case 6: return ColumnSix;
+                    case 7: return ColumnSeven;
+                    case 8: return ColumnEight;
+                    case 9: return ColumnNine;
+                default: return null;
+            }
+        }
+    }
+}
---
+++ AOC_05/Program.cs
@@ -0,0 +1,86 @@
+﻿using AOC_05;
+
+var exampleFile = @"..\\..\\..\\resources\\example_input.txt";
+var file = @"..\\..\\..\\resources\\input.txt";
+string line;
+List<List<int>> listOfInstructions = new List<List<int>>();
+BoxStorage boxStorage = new BoxStorage(9, 100);
+
+
+try
+{
+    using (var reader = new StreamReader(file))
+    {
+        while((line = reader.ReadLine()) != null)
+        {
+            string[] parts = line.Split(' ');
+            List<int> instructions = new List<int>();
+            foreach(var part in parts)
+            {
+                if(int.TryParse(part, out int result))
+                {
+                    instructions.Add(result);
+                }
+            }
+            listOfInstructions.Add(instructions);
+        }
+    }
+    // PartOneSolution();
+    PartTwoSolution();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+void PartOneSolution()
+{
+    foreach(var instruction in listOfInstructions)
+    {
+        for(int i = 0; i < instruction[0]; i++)
+        {
+            var columnToMoveFrom = boxStorage.GetCorrectColumn(instruction[1]);
+            var columnToMoveTo = boxStorage.GetCorrectColumn(instruction[2]);
+            var boxToMove = columnToMoveFrom[columnToMoveFrom.Count - 1];
+            columnToMoveTo.Add(boxToMove);
+            columnToMoveFrom.RemoveAt(columnToMoveFrom.Count - 1);
+        }
+    }
+}
+
+void PartTwoSolution()
+{
+    foreach(var instruction in listOfInstructions)
+    {
+        var tempList = new List<string>();
+        var numberOfBoxesToMove = instruction[0];
+        var columnToMoveFrom = boxStorage.GetCorrectColumn(instruction[1]);
+        var columnToMoveTo = boxStorage.GetCorrectColumn(instruction[2]);
+
+        for (int i = 0; i < numberOfBoxesToMove; i++)
+        {
+            var boxToMove = columnToMoveFrom[columnToMoveFrom.Count - 1];
+            columnToMoveFrom.RemoveAt(columnToMoveFrom.Count - 1);
+            tempList.Add(boxToMove);
+        }
+
+        tempList.Reverse();
+
+        foreach (var entity in tempList)
+        {
+            columnToMoveTo.Add(entity);
+        }
+    }
+}
+
+
+
+Console.WriteLine(boxStorage.GetCorrectColumn(1).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(2).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(3).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(4).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(5).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(6).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(7).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(8).Last());
+Console.WriteLine(boxStorage.GetCorrectColumn(9).Last());
