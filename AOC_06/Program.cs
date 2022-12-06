7420a128e4a43e21ce5e2614bedff5ad860240c9
DanielRiddersporre <daniel.riddersporre@icloud.comgit config --global user.name DanielRiddersporregit config --global user.email daniel.riddersporre@icloud.com>
2022-12-06 18:12:11 +0100

    Day 6 is done and I am very happy with the result.

---
+++ AOC_06/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+
+var file = "C:\\Users\\Danie\\source\\repos\\AdventOfCode2022\\AOC_06\\resources\\file.txt";
+
+try
+{
+    using (var reader = new StreamReader(file))
+    {
+        char[] line = reader.ReadLine().ToCharArray();
+
+        CheckSolutionOne(line);
+        CheckSolutionTwo(line);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+void CheckSolutionOne(char[] line)
+{
+    for(int i = 4; i < line.Length; i++)
+    {
+        var listOfCharacters = line[(i - 4)..i];
+        if(listOfCharacters.Distinct().Count() == 4)
+        {
+            Console.WriteLine($"Answer ONE is: {i}");
+            break;
+        }
+    }
+}
+
+void CheckSolutionTwo(char[] line)
+{
+    for (int i = 14; i < line.Length; i++)
+    {
+        var listOfCharacters = line[(i - 14)..i];
+        if (listOfCharacters.Distinct().Count() == 14)
+        {
+            Console.WriteLine($"Answer TWO is: {i}");
+            break;
+        }
+    }
+}
